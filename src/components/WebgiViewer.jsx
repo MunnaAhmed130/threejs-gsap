43daa6e65ec1f806354afd6311c2ba396e8ace31
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-31 14:21:46 +0600

    commit

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useCallback, useEffect } from "react";
 import {
     ViewerApp,
     AssetManagerPlugin,
@@ -9,12 +9,80 @@ import {
     SSAOPlugin,
     BloomPlugin,
     GammaCorrectionPlugin,
-    addBasePlugins,
     mobileAndTabletCheck,
 } from "webgi";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { scrollAnimation } from "../lib/scroll-animation";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const WebgiViewer = () => {
     const canvasRef = useRef(null);
+
+    const memorizeScrollAnimation = useCallback(
+        async (position, target, onUpdate) => {
+            if (position && target && onUpdate) {
+                scrollAnimation(position, target, onUpdate);
+            }
+        }
+    );
+
+    const setupViewer = useCallback(async () => {
+        // Initialize the viewer
+        const viewer = new ViewerApp({
+            canvas: canvasRef.current,
+        });
+
+        // Add some plugins
+        const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+        const camera = viewer.scene.activeCamera;
+        const position = camera.position;
+        const target = camera.target;
+
+        // Add a popup(in HTML) with download progress when any asset is downloading.
+        // await viewer.addPlugin(AssetManagerBasicPopupPlugin);
+
+        // Add plugins individually.
+        await viewer.addPlugin(GBufferPlugin);
+        await viewer.addPlugin(new ProgressivePlugin(32));
+        await viewer.addPlugin(new TonemapPlugin(true));
+        await viewer.addPlugin(GammaCorrectionPlugin);
+        await viewer.addPlugin(SSRPlugin);
+        await viewer.addPlugin(SSAOPlugin);
+        await viewer.addPlugin(BloomPlugin);
+
+        // This must be called once after all plugins are added.
+        viewer.renderer.refreshPipeline();
+
+        await manager.addFromPath("scene-black.glb");
+
+        viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+
+        viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+
+        window.scrollTo(0, 0);
+
+        let needsUpdate = true;
+
+        const onUpdate = () => {
+            needsUpdate = true;
+            viewer.setDirty();
+        };
+        viewer.addEventListener("preFrame", () => {
+            if (needsUpdate) {
+                camera.positionTargetUpdated(true);
+                needsUpdate = false;
+            }
+        });
+
+        memorizeScrollAnimation(position, target, onUpdate);
+    }, []);
+
+    useEffect(() => {
+        setupViewer();
+    }, []);
 
     return (
         <section id="webgi-canvas-container" className="">
